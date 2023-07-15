5a0db52194163f2e520217aceca562496f89a154
intelagense <intelagense@gmail.com>
2023-07-14 21:01:31 -0500

    Adds scheduler and player movement control

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 let Game = {
     display: null,
+    engine: null,
 
     init: function () {
         this.display = new ROT.Display();
         document.body.appendChild(this.display.getContainer());
         this._generateMap();
+        let scheduler = new ROT.Scheduler.Simple();
+        scheduler.add(this.player, true);
+        this.engine = new ROT.Engine(scheduler);
+        this.engine.start();
     }
 }
 
@@ -14,11 +19,11 @@ Game._generateMap = function () {
     let freeCells = [];
 
     let digCallback = function (x, y, value) {
-        if (value) { return } //do not store walls
+        if (value) { return; } //do not store walls
 
         let key = `${x},${y}`
         freeCells.push(key);
-        this.map[key] = ".";
+        this.map[key] = "⢕";
     }
     digger.create(digCallback.bind(this));
 
@@ -67,3 +72,40 @@ Game._createPlayer = function (freeCells) {
     let y = parseInt(parts[1]);
     this.player = new Player(x, y);
 }
+
+Player.prototype.act = function () {
+    Game.engine.lock();
+    window.addEventListener("keydown", this);
+}
+
+Player.prototype.handleEvent = function (e) {
+    e.preventDefault() //prevents arrow keys from functioning
+    let keyMap = {}
+    keyMap[33] = 1; // PageUp
+    keyMap[34] = 3; // PageDown
+    keyMap[35] = 5; // End
+    keyMap[36] = 7; // Home
+    keyMap[37] = 6; // ArrowLeft
+    keyMap[38] = 0; // ArrowUp
+    keyMap[39] = 2; // ArrowRight
+    keyMap[40] = 4; // ArrowDown
+
+    let code = e.keyCode;
+
+    if (!(code in keyMap)) { return; }
+
+    let diff = ROT.DIRS[8][keyMap[code]];
+    let newX = this._x + diff[0];
+    let newY = this._y + diff[1];
+
+    let newKey = `${newX},${newY}`;
+    if (!(newKey in Game.map)) { return; }
+
+    Game.display.draw(this._x, this._y, Game.map[`${this._x},${this._y}`]);
+    this._x = newX;
+    this._y = newY;
+    this._draw();
+    window.removeEventListener("keydown", this);
+    Game.engine.unlock()
+}
+
